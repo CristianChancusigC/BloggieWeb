4e9e34189a22bd14a7c06f5197dea34d67f71064
Cristian Chancusig <cristian@quyka.net>
2023-07-25 18:52:43 -0500

    Crud, Add

--- BloggieWeb/Controllers/AdminTagsController.cs
+++ BloggieWeb/Controllers/AdminTagsController.cs
@@ -1,10 +1,19 @@
-﻿using BloggieWeb.Models.ViewModels;
+﻿using BloggieWeb.Data;
+using BloggieWeb.Models.Domain;
+using BloggieWeb.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BloggieWeb.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -16,8 +25,15 @@ namespace BloggieWeb.Controllers
         public IActionResult Add(AddTagRequest addTagRequest)
         {
             //var name = Request.Form["name"];
-            var name = addTagRequest.Name;
-            var displayname = addTagRequest.DisplayName;
+            //var displayname = addTagRequest.DisplayName;
+            // Mapping AddTsgReques to Tag Domian model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
             return View("Add");
         }
     }
